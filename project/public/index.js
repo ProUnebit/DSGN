28b795aa4e87fbccd159b89f89be579d56e42324
ProUnebit <ProUnebit@yandex.ru>
2018-04-12 14:45:10 +0300

    add JS for section "header"

--- project/public/index.js
+++ project/public/index.js
@@ -1,1 +1,27 @@
-console.log("It's work!");
+// All the selectors
+const btnOpenMenu = document.querySelector('.header__burger-pic');
+const btnCloseMenu = document.querySelector('.header__times-pic');
+const headerMenu = document.querySelector('.header__menu');
+
+// BLOCKS
+// Header
+// global close menu function
+function closeMenu(e) {
+    if (!e.target.matches('.header__menu, .header__menu *')) {
+    headerMenu.style.display = '';
+    this.removeEventListener('click', closeMenu);
+    }
+}
+// open menu
+btnOpenMenu.addEventListener('click', () => {
+    headerMenu.style.animation = 'open-menu .35s ease';
+    headerMenu.style.display = 'flex';
+    setTimeout(() => { document.addEventListener('click', closeMenu) }, 350);
+});
+// close menu
+btnCloseMenu.addEventListener('click', () => {
+    headerMenu.style.display = '';
+    document.removeEventListener('click', closeMenu);
+});
+
+// Projects
