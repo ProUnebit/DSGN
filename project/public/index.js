79ce7c6f2f0ad3e251c812207fea0c4dd7af269b
ProUnebit <ProUnebit@yandex.ru>
2018-04-17 20:33:15 +0300

    add initial sorting for section "projects" (index.js)

--- project/public/index.js
+++ project/public/index.js
@@ -2,6 +2,11 @@
 const btnOpenMenu = document.querySelector('.header__burger-pic');
 const btnCloseMenu = document.querySelector('.header__times-pic');
 const headerMenu = document.querySelector('.header__menu');
+const timeline_2017 = document.querySelector('.projects__timeline-item--2017');
+const timeline_2015 = document.querySelector('.projects__timeline-item--2015');
+const timeline_2013 = document.querySelector('.projects__timeline-item--2013');
+const timeline_2012 = document.querySelector('.projects__timeline-item--2012');
+const timeline_2010 = document.querySelector('.projects__timeline-item--2010');
 
 // BLOCKS
 // Header
@@ -25,3 +30,36 @@ btnCloseMenu.addEventListener('click', () => {
 });
 
 // Projects
+let projectsAll = [
+    {name: 'fondue', typology: 'suspension lamp', year: '2015'},
+    {name: 'louis xx', typology: 'chairs', year: '2012'},
+    {name: '395-396 p22', typology: 'arimchairs', year: '2013'},
+    {name: 'sesann', typology: 'arimchairs', year: '2010'},
+    {name: 'alessi' , typology: 'decor', year: '2017'},
+    {name: 'potter', typology: 'accessory', year: '2010'},
+    {name: 'tabano', typology: 'arimchairs', year: '2015'},
+    {name: 'fiji', typology: 'arimchairs', year: '2015'}
+];
+
+let projects_2017 = [];
+let projects_2015 = [];
+let projects_2013 = [];
+let projects_2012 = [];
+let projects_2010 = [];
+
+timeline_2015.addEventListener('click', () => {
+    for ( var i = 0; i < projectsAll.length; i++ ) {
+        if (projectsAll[i].year === '2015') {
+            projects_2015.push(projectsAll[i]);
+        }
+    }
+    for ( let x = 0; x < projects_2015.length; x++ ) {
+        let varSelector = document.querySelector(`.projects__product-miniature--${x+1}`);
+        varSelector.innerHTML = projects_2015[x].name;
+    }
+
+    for ( let y = projects_2015.length; y < projectsAll.length; y++ ) {
+        let varSelector2 = document.querySelector(`.projects__product-miniature--${y+1}`);
+        varSelector2.style.visibility = "hidden";
+    }
+});
